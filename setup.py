d5ac191afa73938cbddde749d7986725b6e1d3af
Chettipalli Karthik <chettipallikarthik150@gmail.com>
2025-10-16 22:48:54 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='youtube',  # You can change this to your project name
+    version='0.0.0',  # Initial version — can be updated as your project evolves
+    author='Karthik Chettipalli',  # Your name
+    author_email='chettipallikarthik150@gmail.com',  # Your email address
+    packages=find_packages(),  # Automatically finds all Python packages in your project
+    install_requires=[]  # List of dependencies (e.g., ['numpy', 'pandas'])
+)
